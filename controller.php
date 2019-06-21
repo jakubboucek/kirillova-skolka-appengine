c46dd2ad4c51269ca2e0885fab909a5d9521c667
Jakub Bouček <pan@jakubboucek.cz>
2019-06-21 16:04:54 +0200

    Add buggy/non-buggy variants

--- controller.php
+++ controller.php
@@ -1,11 +1,18 @@
 <?php
 declare(strict_types=1);
 
-switch ($_SERVER['REQUEST_URI']) {
-    case '/data/items.json':
-        require __DIR__ . '/data/items-json.php';
-        break;
-    default:
-        http_response_code(404);
-        exit('Not Found');
+
+$uri = $_SERVER['REQUEST_URI'];
+
+if($uri === '/data/items.json') {
+	$buggy = false;
+	return require __DIR__ . '/data/items-json.php';
 }
+
+if($uri === '/data/items.buggy.json') {
+	$buggy = true;
+	return require __DIR__ . '/data/items-json.php';
+}
+
+http_response_code(404);
+exit('Not Found');

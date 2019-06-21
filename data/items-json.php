cee4c8e8ab3fe660b101e0dfa20587d9c5a0191c
Jakub Bouček <pan@jakubboucek.cz>
2019-06-21 15:56:04 +0200

    Add simultaneous random bugs

--- data/items-json.php
+++ data/items-json.php
@@ -4,6 +4,24 @@ declare(strict_types=1);
 header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
 header('Cache-Control: no-store, no-cache, must-revalidate');
 
+$random = random_int(0, 10);
+
+// Bug 1 - Fatal error
+if($random === 0) {
+	throw new Exception("Error Processing Request", 1);
+}
+
+// Bug 2 - string (invalid JSON)
+if($random === 1) {
+	die("ahoj");
+}
+
+// Bug 3 - string (valid JSON)
+if($random === 2) {
+	die("\"ahoj\"");
+}
+
+// in other cases send regular JSON data
 header('Content-type: application/json; charset=utf-8');
 $items = [
     "0" => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
